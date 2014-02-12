7364fc52e7dafa5d9919d0140d9d76a5527a593a
lucasBertola <1618004@supinfo.com>
2014-02-12 22:26:22 +0100

    Edit Readme + rangement

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
-HTTP-CLIENT
+
 ===========
 
 This is an implementation of an HTTP client in c++.
 
 ===========
 By Lucas Bertola  
+
+
+HTTP-CLIENT
+=======================================
+:Author:    Lucas Bertola  
+:Email:     161804_dot_supinfo_dot_com
+
+Introduction
+------------
+HTTP-CLIENT is an implementation of an HTTP client in c++.
+
+
+Features
+--------
+
+Here is a list of features:
+
+* Request Get
+* Request Post
+* Cookie
+* User-agent
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
 #include "CoucheHttp.h"
-#include "rechercheInformation.h"
-#include <sstream>
-#include <math.h>
-#include "excel.h"
-using namespace std;
 
 int main()
 {
     Header head;
-    head.setHost("http://travian.fr");
+    head.setHost("http://twitter.github.io");
     head.setPath("/");
 
     head.setMethod("GET");
@@ -17,12 +12,16 @@ int main()
     head.setAccept("text/html,application/xhtml+xml,application/xml;q=0.9,*//*;q=0.8");
     head.setUserAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:21.0) Gecko/20100101 Firefox/21.0");
     head.setAccept_language("fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3");
-    head.setAccept_encoding("gzip, deflate");
+
+    //Todo implementer le gzip
+    //head.setAccept_encoding("gzip, deflate");
     head.setConnexion("keep-alive");
 
     Http* http=new Http(head);
 
-    std::string page=http->getPage(&head);
+    std::string page = http->getPage(&head);
+
     std::cout<<page<<std::endl;
+
     return 0;
 }
